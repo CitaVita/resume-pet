4b3cdea9cba73d6c266b5326e404a01803274732
CitaVita <citavita704@gmail.com>
2023-09-12 20:36:28 +0100

    done

--- src/route/index.js
+++ src/route/index.js
@@ -323,7 +323,7 @@ router.get('/person', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('person', {
     
-layout: 'person',
+layout: 'basic',
 
     person: {
       name: 'Emma Johnson',
@@ -430,7 +430,7 @@ router.get('/bio', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('bio', {
 
-    layout: 'bio',
+    layout: 'basic',
 
     name: 'Albert Einstein',
     birthdate: 'March 14, 1879',
@@ -532,7 +532,7 @@ router.get('/program', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('program', {
 
-    layout: 'program',
+    layout: 'basic',
     
     program: {
       excursion: {
