fd8ba442ea0ab1e5b0a5a7f975beafcb93acd882
CitaVita <citavita704@gmail.com>
2023-11-19 21:21:07 +0000

    shoprewiev

--- src/route/index.js
+++ src/route/index.js
@@ -2318,11 +2318,11 @@ router.get('/shopprofile', function (req, res) {
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/shopreview', function (req, res) {
+router.get('/shoprewiev', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('shopreview', {
+  res.render('git ', {
     // layout: 'shop',
     navigation: {
       links: [
